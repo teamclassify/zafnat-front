8fe6ef8dce23ab5efaf13da1f9d536cd333ba0ff
Andres Parra <byandrev@gmail.com>
2024-10-28 15:40:59 -0500

    Refactor Title component to remove unnecessary text-slate-700 class

--- src/components/custom/Title.jsx
+++ src/components/custom/Title.jsx
@@ -1,6 +1,6 @@
 export function Title({ title }) {
   return (
-    <h3 className="scroll-m-20 pb-2 pt-6 text-2xl font-semibold tracking-tight first:mt-0 text-slate-700">
+    <h3 className="scroll-m-20 pb-2 pt-6 text-2xl font-semibold tracking-tight first:mt-0">
       {title}
     </h3>
   );
