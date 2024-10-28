d33a89a507b32bc188a4da6948fcc591ee1af343
Andres Parra <byandrev@gmail.com>
2024-10-28 15:39:32 -0500

    feat: Add Footer component to DefaultTemplate

--- src/components/templates/DefaultTemplate.jsx
+++ src/components/templates/DefaultTemplate.jsx
@@ -1,3 +1,4 @@
+import Footer from "../custom/Footer";
 import Header from "../custom/Header";
 
 function DefaultTemplate({ children }) {
@@ -6,6 +7,8 @@ function DefaultTemplate({ children }) {
       <Header className="w-[90%] max-w-screen-lg mx-auto" />
 
       <main className="py-8 mx-auto max-w-screen-lg">{children}</main>
+
+      <Footer />
     </>
   );
 }
