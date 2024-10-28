4f1f97c1dfad07befe7cd68653c127c2afa4d772
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-10-28 12:33:51 -0500

    feat: add reviews count

--- src/components/custom/AccordionFilter.jsx
+++ src/components/custom/AccordionFilter.jsx
@@ -8,7 +8,7 @@ import { CheckboxDemo } from "./CheckBoxDemo";
 
 export default function AccordionFilter() {
   return (
-    <Accordion type="single" className="w-2/4" collapsible>
+    <Accordion type="single" className="w-2/4">
       <AccordionItem value="item-1">
         <AccordionTrigger>Categoria</AccordionTrigger>
         <AccordionContent className="flex flex-col gap-3">
@@ -17,7 +17,7 @@ export default function AccordionFilter() {
           <CheckboxDemo name="Jeans" />
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-2">
+      <AccordionItem value="item-1">
         <AccordionTrigger>Color</AccordionTrigger>
         <AccordionContent className="flex flex-col gap-3">
           <CheckboxDemo name="Azul" />
@@ -25,7 +25,7 @@ export default function AccordionFilter() {
           <CheckboxDemo name="Amarillo" />
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-3">
+      <AccordionItem value="item-1">
         <AccordionTrigger>Talla</AccordionTrigger>
         <AccordionContent className="flex flex-col gap-3">
           <CheckboxDemo name="6" />
@@ -37,7 +37,7 @@ export default function AccordionFilter() {
           <CheckboxDemo name="18" />
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-4">
+      <AccordionItem value="item-1">
         <AccordionTrigger>Modelo</AccordionTrigger>
         <AccordionContent>
           Yes. It adheres to the WAI-ARIA design pattern.
--- src/components/custom/Product.jsx
+++ src/components/custom/Product.jsx
@@ -1,8 +1,11 @@
-export default function Product({ image, /*reviews,*/  name, price }) {
+export default function Product({ image, reviews, name, price }) {
   return (
     <div className="flex flex-col items-center justify-center">
       <img src={image} alt="image de jean" className="w-4/5" />
-      <p>⭐⭐⭐⭐⭐</p>
+      <div className="flex items-center gap-2">
+        <p>⭐⭐⭐⭐⭐</p>
+        <p className="text-slate-500 text-xs">({reviews.length})</p>
+      </div>
       <p>{name}</p>
       <p className="font-semibold">${price}.000</p>
     </div>
