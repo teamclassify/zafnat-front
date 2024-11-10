d194be9aae869093b158094eea783dc027d8457b
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-10 16:58:38 -0500

    feat: add some shipping method

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -1,14 +1,18 @@
-import { PaymentMethod } from "../components/custom/PaymentMethod";
 import Purchase from "../components/custom/Purchase";
+import ShippingInfo from "../components/custom/ShippingInfo";
 import { Title } from "../components/custom/Title";
 import DefaultTemplate from "../components/templates/DefaultTemplate";
 
 export default function Shipping() {
   return (
     <DefaultTemplate>
-      <Title title={"Metodo de envío"} />
+      <Title title="Metodo de envío" />
       <div className="flex flex-row justify-between gap-3">
-        <PaymentMethod />
+        <div className="flex flex-col pt-4 h-1/2 w-2/4 gap-4 ">
+          <ShippingInfo title={"Recogida en tienda"} description={"Gratis"} icon={"Store"}/>
+          <ShippingInfo title={"Envíos Nacionales"} description={"Depende de la zona"} icon={"Truck"} />
+          <ShippingInfo title={"Envíos Internacionales"} description={"Depende de la zona"} icon={"Airplane"} />
+        </div>
         <Purchase buy={true} />
       </div>
     </DefaultTemplate>
