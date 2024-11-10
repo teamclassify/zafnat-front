697c99b53189215556885d07ac65d3708de69bfb
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-10 00:08:34 -0500

    feat: add new component PaymentMethod

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -1,3 +1,5 @@
+import { PaymentMethod } from "../components/custom/PaymentMethod";
+import Purchase from "../components/custom/Purchase";
 import { Title } from "../components/custom/Title";
 import DefaultTemplate from "../components/templates/DefaultTemplate";
 
@@ -5,7 +7,10 @@ export default function Shipping() {
   return (
     <DefaultTemplate>
       <Title title={"Metodo de envío"} />
-      
+      <div className="flex flex-row justify-between gap-3">
+        <PaymentMethod />
+        <Purchase buy={true} />
+      </div>
     </DefaultTemplate>
   );
 }
