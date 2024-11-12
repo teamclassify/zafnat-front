5126b3a515b6c516779bd809fdbe73bd7ef53f92
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-12 12:02:53 -0500

    feat: add icon Truck

--- src/pages/PaymentDone.jsx
+++ src/pages/PaymentDone.jsx
@@ -1,10 +1,12 @@
+import { Truck } from "lucide-react";
 import { Title } from "../components/custom/Title";
 import DefaultTemplate from "../components/templates/DefaultTemplate";
 
 export default function PaymentDone() {
   return (
     <DefaultTemplate>
-      <main>
+      <main className="flex flex-col justify-between items-center"> 
+        <Truck />
         <Title title="Gracias por tu pedido" />
       </main>
     </DefaultTemplate>
