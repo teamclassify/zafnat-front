66cfa7f2db9380c8504dbd7becc8d3d90c2b7870
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-14 08:25:17 -0500

    feat: add switch to social media options

--- src/pages/SocialMedia.jsx
+++ src/pages/SocialMedia.jsx
@@ -1,15 +1,17 @@
+import { CardSocialMedia } from "../components/custom/CardSocialMedia";
 import { Title } from "../components/custom/Title";
 import AdminTemplate from "../components/templates/AdminTemplate";
 
 export default function SocialMedia() {
   return (
     <AdminTemplate>
-      <main>
+      <main className="flex flex-col gap-5">
         <Title title="Redes Sociales" />
         <p>
           Configure los ajustes de redes sociales para permitir compartir
-          productos en Facebook, Whatsapp, Telegram, Instagram y TikTok{" "}
+          productos en Facebook, Whatsapp, Telegram, Instagram y TikTok
         </p>
+        <CardSocialMedia />
       </main>
     </AdminTemplate>
   );
