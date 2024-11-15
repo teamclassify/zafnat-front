e4dc4102796cd511e2f1058591719aae790fc208
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-14 21:55:24 -0500

    feat: change flex position

--- src/pages/AlertStockPage.jsx
+++ src/pages/AlertStockPage.jsx
@@ -2,16 +2,16 @@ import AdminTemplate from "../components/templates/AdminTemplate";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card } from "@/components/ui/card";
+import { Button } from "../components/ui/button";
+import { Title } from "../components/custom/Title";
 
 export default function AlertStockPage() {
   return (
     <>
       <AdminTemplate>
         <div>
-          <h3 className="scroll-m-20 pb-2 pt-6 text-2xl font-semibold tracking-tight first:mt-0 text-slate-700">
-            Ajustes de Inventario
-          </h3>
-          <div className="flex justify-between gap-4 pt-4">
+          <Title title="Ajustes de Inventario" />
+          <div className="flex justify-between pt-2 w-4/5">
             <div>
               <h2 className="font-medium">Alerta bajo Stock</h2>
               <p className=" text-gray-800">
@@ -20,9 +20,12 @@ export default function AlertStockPage() {
               </p>
             </div>
             <Card className="p-7">
-              <div className="flex flex-col w-80 gap-2 ">
+              <div className="flex flex-col w-80 gap-4 ">
                 <Label>Valor *</Label>
                 <Input type="number" placeholder="0" />
+                <div className="flex flex-row justify-end">
+                  <Button>Establecer</Button>
+                </div>
               </div>
             </Card>
           </div>
