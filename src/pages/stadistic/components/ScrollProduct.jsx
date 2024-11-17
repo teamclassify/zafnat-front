5d783a2f1c81a2f90aca98b565b45565aeee29a3
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-16 22:18:12 -0500

    feat: add progress component ui

--- src/pages/stadistic/components/ScrollProduct.jsx
+++ src/pages/stadistic/components/ScrollProduct.jsx
@@ -1,5 +1,7 @@
-export default function ScrollProduct({product}){
-  return(
-    <p>{product.name}</p>
-  )
+export default function ScrollProduct({ product }) {
+  return (
+    <div>
+      <p>{product.name}</p>
+    </div>
+  );
 }
