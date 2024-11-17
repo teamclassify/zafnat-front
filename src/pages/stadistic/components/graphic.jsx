367cdd37d48a0788813f1803f36ef171f3036d6e
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-16 20:56:31 -0500

    feat: add chart component in page

--- src/pages/stadistic/components/graphic.jsx
+++ src/pages/stadistic/components/graphic.jsx
@@ -5,18 +5,22 @@ import {
   CardHeader,
   CardTitle,
 } from "../../../components/ui/card";
+import { Stadistic } from "./stadistic";
 
 export default function Graphic({ title }) {
   return (
     <Card>
       <CardHeader>
-        <div className="flex justify-between">
+        <div className="flex flex-col justify-between gap-4">
           <CardTitle>{title}</CardTitle>
+          <CardTitle className="text-4xl">149</CardTitle>
         </div>
-        <CardDescription>1234</CardDescription>
+        <CardDescription>+10.5% respecto al mes pasado</CardDescription>
       </CardHeader>
 
-      <CardContent></CardContent>
+      <CardContent>
+        <Stadistic />
+      </CardContent>
     </Card>
   );
 }
--- src/pages/stadistic/product.jsx
+++ src/pages/stadistic/product.jsx
@@ -2,11 +2,18 @@ import { Title } from "../../components/custom/Title";
 import AdminTemplate from "../../components/templates/AdminTemplate";
 import Graphic from "./components/graphic";
 
-export default function ProductStadistic(){
-  return(
+export default function ProductStadistic() {
+  return (
     <AdminTemplate>
-      <Title title="Estadisticas de productos"/>
-      <Graphic title="Productos vendidos"/>
+      <Title title="Estadisticas de productos" />
+      <div className="grid grid-cols-2 gap-5">
+        <Graphic title="Productos vendidos" />
+        <Graphic title="Productos vendidos" />
+        <Graphic title="Productos vendidos" />
+        <Graphic title="Productos vendidos" />
+        <Graphic title="Productos vendidos" />
+        <Graphic title="Productos vendidos" />
+      </div>
     </AdminTemplate>
-  )
+  );
 }
