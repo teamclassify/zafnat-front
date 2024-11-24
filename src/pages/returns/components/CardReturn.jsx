b14eaca60349b0d043c90929f5595883d77f563f
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-24 18:34:21 -0500

    feat(component): add Notification for users

--- src/pages/returns/components/CardReturn.jsx
+++ src/pages/returns/components/CardReturn.jsx
@@ -6,10 +6,13 @@ import {
 } from "@/components/ui/card";
 import { OptionButton } from "../../../components/custom/OptionsButton";
 import { Badge } from "@/components/ui/badge";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { DialogNotification } from "./Notification";
 
 export function CardReturn({ client }) {
   const [status, setStatus] = useState(client.status);
+  const [isOpen, setIsOpen] = useState(false);
+
   const getBadgeColor = (status) => {
     switch (status) {
       case "Completado":
@@ -25,34 +28,44 @@ export function CardReturn({ client }) {
     }
   };
 
-  return (
-    <Card>
-      <CardHeader>
-        <div className="flex justify-between items-center">
-          <CardTitle className="text-lg font-semibold">
-            Cliente: {client.customer}
-          </CardTitle>
-          <Badge className={getBadgeColor(status)}>{status}</Badge>
-          <OptionButton
-            options={["Pendiente", "En proceso", "Completado", "Rechazado"]}
-            setStatus={setStatus}
-          />
-        </div>
+  useEffect(() => {
+    if (status !== client.status) {
+      setIsOpen(true);
+    }
+  }, [status, client.status]);
 
-        <div className="mt-4 space-y-2">
-          <CardDescription className="font-semibold">
-            <span className="text-gray-500">Producto:</span>{" "}
-            {client.returnedProduct.type} - {client.returnedProduct.color},
-            Talla {client.returnedProduct.size}
-          </CardDescription>
-          <CardDescription className="font-semibold">
-            <span className="text-gray-500">Fecha:</span> {client.date}
-          </CardDescription>
-          <CardDescription className="font-semibold">
-            <span className="text-gray-500">Motivo:</span> {client.reason}
-          </CardDescription>
-        </div>
-      </CardHeader>
-    </Card>
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <div className="flex justify-between items-center">
+            <CardTitle className="text-lg font-semibold">
+              Cliente: {client.customer}
+            </CardTitle>
+            <Badge className={getBadgeColor(status)}>{status}</Badge>
+            <OptionButton
+              options={["Pendiente", "En proceso", "Completado", "Rechazado"]}
+              setStatus={setStatus}
+            />
+          </div>
+
+          <div className="mt-4 space-y-2">
+            <CardDescription className="font-semibold">
+              <span className="text-gray-500">Producto:</span>{" "}
+              {client.returnedProduct.type} - {client.returnedProduct.color},
+              Talla {client.returnedProduct.size}
+            </CardDescription>
+            <CardDescription className="font-semibold">
+              <span className="text-gray-500">Fecha:</span> {client.date}
+            </CardDescription>
+            <CardDescription className="font-semibold">
+              <span className="text-gray-500">Motivo:</span> {client.reason}
+            </CardDescription>
+          </div>
+        </CardHeader>
+      </Card>
+
+      <DialogNotification isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
   );
 }
