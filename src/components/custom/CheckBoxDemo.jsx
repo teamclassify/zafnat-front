4830df9bc2fbfdbbfb7e1aefa71934eb755c3743
Andres Parra <byandrev@gmail.com>
2024-11-24 18:06:49 -0500

    fix: capitalize label text in CheckboxDemo component

--- src/components/custom/CheckBoxDemo.jsx
+++ src/components/custom/CheckBoxDemo.jsx
@@ -4,9 +4,7 @@ export function CheckboxDemo({ name, onChange, filter }) {
   return (
     <div className="flex items-center space-x-2">
       <Checkbox id="terms" onCheckedChange={() => onChange(name, filter)} />
-      <label
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-      >
+      <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 capitalize">
         {name}
       </label>
     </div>
