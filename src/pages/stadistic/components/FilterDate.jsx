e770f34ddf2709ac98f5c1041bda68896a4bc2f0
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-25 21:49:25 -0500

    refactor: Add context for filter management in sales statistics module

--- src/pages/stadistic/components/FilterDate.jsx
+++ src/pages/stadistic/components/FilterDate.jsx
@@ -5,36 +5,40 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useFilterContext } from "../../../context/FilterStadisticContext";
+
 
 export default function FilterDate() {
+  const { timeRange, setTimeRange } = useFilterContext();
+
   return (
     <Select value={timeRange} onValueChange={setTimeRange}>
-            <SelectTrigger
-              className="w-[160px] rounded-lg sm:ml-auto"
-              aria-label="Select a value"
-            >
-              <SelectValue placeholder="Últimos 3 meses" />
-            </SelectTrigger>
-            <SelectContent className="rounded-xl">
-              <SelectItem value="all" className="rounded-lg">
-                Todos los registros
-              </SelectItem>
-              <SelectItem value="5y" className="rounded-lg">
-                Últimos 5 años
-              </SelectItem>
-              <SelectItem value="1y" className="rounded-lg">
-                Último año
-              </SelectItem>
-              <SelectItem value="6m" className="rounded-lg">
-                Últimos 6 meses
-              </SelectItem>
-              <SelectItem value="90d" className="rounded-lg">
-                Últimos 3 meses
-              </SelectItem>
-              <SelectItem value="30d" className="rounded-lg">
-                Últimos 30 días
-              </SelectItem>
-            </SelectContent>
-          </Select>
-  )
+      <SelectTrigger
+        className="w-[160px] rounded-lg sm:ml-auto"
+        aria-label="Select a value"
+      >
+        <SelectValue placeholder="Últimos 3 meses" />
+      </SelectTrigger>
+      <SelectContent className="rounded-xl">
+        <SelectItem value="all" className="rounded-lg">
+          Todos los registros
+        </SelectItem>
+        <SelectItem value="5y" className="rounded-lg">
+          Últimos 5 años
+        </SelectItem>
+        <SelectItem value="1y" className="rounded-lg">
+          Último año
+        </SelectItem>
+        <SelectItem value="6m" className="rounded-lg">
+          Últimos 6 meses
+        </SelectItem>
+        <SelectItem value="90d" className="rounded-lg">
+          Últimos 3 meses
+        </SelectItem>
+        <SelectItem value="30d" className="rounded-lg">
+          Últimos 30 días
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
 }
--- src/pages/stadistic/sales.jsx
+++ src/pages/stadistic/sales.jsx
@@ -2,105 +2,19 @@ import { Title } from "../../components/custom/Title";
 import AdminTemplate from "../../components/templates/AdminTemplate";
 import Graphic from "./components/graphic";
 import { graphicDataSales } from "../../hooks/useDataGraphic";
-import { useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import FilterDate from "./components/FilterDate";
+import { useFilterContext } from "../../context/FilterStadisticContext";
 
 export default function SalesStadistic() {
   const value = ["ingresos", "ventas", "pedidos"];
-  const [timeRange, setTimeRange] = useState("90d");
-
-  const monthMapping = {
-    Enero: 0,
-    Febrero: 1,
-    Marzo: 2,
-    Abril: 3,
-    Mayo: 4,
-    Junio: 5,
-    Julio: 6,
-    Agosto: 7,
-    Septiembre: 8,
-    Octubre: 9,
-    Noviembre: 10,
-    Diciembre: 11,
-  };
-
-  const filterDataByTimeRange = (data) => {
-    const referenceDate = new Date();
-    let monthsToSubtract = 3;
-
-    switch (timeRange) {
-      case "90d":
-        monthsToSubtract = 3;
-        break;
-      case "30d":
-        monthsToSubtract = 1;
-        break;
-      case "6m":
-        monthsToSubtract = 5;
-        break;
-      case "1y":
-        monthsToSubtract = 12;
-        break;
-      case "5y":
-        monthsToSubtract = 60;
-        break;
-      case "all":
-        return data;
-      default:
-        monthsToSubtract = 3;
-    }
-
-    const startDate = new Date(
-      referenceDate.getFullYear(),
-      referenceDate.getMonth() - monthsToSubtract
-    );
-
-    return data.filter((item) => {
-      const itemMonthIndex = monthMapping[item.month];
-      const itemDate = new Date(referenceDate.getFullYear(), itemMonthIndex, 1);
-      return itemDate >= startDate;
-    });
-  };
+  const { filterDataByTimeRange } = useFilterContext();
 
   return (
     <AdminTemplate>
       <Title title="Estadísticas de ventas" />
       <div className="mb-5">
         <div className="flex justify-end mb-4">
-          <Select value={timeRange} onValueChange={setTimeRange}>
-            <SelectTrigger
-              className="w-[160px] rounded-lg sm:ml-auto"
-              aria-label="Select a value"
-            >
-              <SelectValue placeholder="Últimos 3 meses" />
-            </SelectTrigger>
-            <SelectContent className="rounded-xl">
-              <SelectItem value="all" className="rounded-lg">
-                Todos los registros
-              </SelectItem>
-              <SelectItem value="5y" className="rounded-lg">
-                Últimos 5 años
-              </SelectItem>
-              <SelectItem value="1y" className="rounded-lg">
-                Último año
-              </SelectItem>
-              <SelectItem value="6m" className="rounded-lg">
-                Últimos 6 meses
-              </SelectItem>
-              <SelectItem value="90d" className="rounded-lg">
-                Últimos 3 meses
-              </SelectItem>
-              <SelectItem value="30d" className="rounded-lg">
-                Últimos 30 días
-              </SelectItem>
-            </SelectContent>
-          </Select>
+          <FilterDate />
         </div>
         <Graphic
           title={graphicDataSales[0].title}
