2cbcd6971521932c503a1c5809840916bb359ee8
Andres Parra <byandrev@gmail.com>
2024-11-25 19:07:49 -0500

    feat: add create function to AddressService for address creation

--- src/services/api/AddressService.jsx
+++ src/services/api/AddressService.jsx
@@ -23,8 +23,36 @@ async function getAll() {
   }
 }
 
+async function create({ address, city, postalCode, country }) {
+  try {
+    const token = await getToken();
+
+    if (!token) throw new Error("Token not found");
+
+    const res = await axios({
+      url: `${URL}/address/create`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        address_line_1: address,
+        city: city,
+        postal_code: postalCode,
+        country: country,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+}
+
 const AddressService = {
   getAll,
+  create,
 };
 
 export default AddressService;
