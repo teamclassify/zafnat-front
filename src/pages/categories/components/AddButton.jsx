67018625406e34dbed6714ce5c3a6771d6e45906
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-11-25 20:10:52 -0500

    feat: add modal in button "add category"

--- src/pages/categories/components/AddButton.jsx
+++ src/pages/categories/components/AddButton.jsx
@@ -1,11 +1,64 @@
 import { IoAddCircleOutline } from "react-icons/io5";
 import { Button } from "../../../components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
 
 export default function AddButton() {
+  const handleSave = () => {
+    toast.success("Se ha creado la categoria exitosamente");
+  };
   return (
-    <Button variant={"outline"}>
-      <IoAddCircleOutline />
-      Agregar categoria
-    </Button>
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="outline">
+          <IoAddCircleOutline />
+          Agregar Categoria
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Agregar Categoria</DialogTitle>
+          <DialogDescription>
+            Ingrese la información requerida para crear la categoria
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Nombre
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Categoria 1"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="description" className="text-right">
+              Descripción
+            </Label>
+            <Input id="description" defaultValue=".." className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose>
+            <Button type="submit" onClick={() => handleSave()}>
+              Guardar cambios
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
