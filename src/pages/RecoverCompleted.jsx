df160b793568cbe4ef873f5c5c504af2bd09e687
Andres Parra <byandrev@gmail.com>
2024-11-26 13:47:40 -0500

    feat: update recovery completion message and redirect to login page

--- src/pages/RecoverCompleted.jsx
+++ src/pages/RecoverCompleted.jsx
@@ -9,11 +9,10 @@ function RecoverPassword() {
       <div className="flex items-center justify-center mt-20 flex-col gap-4">
         <LuShieldCheck size={100} />
         <h3 className="self-center text-center">
-          Enviamos un correo a {/*Poner correo aqui y buscar como censurarlo*/}{" "}
-          ex******@gmail.com. <br /> Ingresa y sigue las instrucciones para
+          Te enviamos un correo. <br /> Ingresa y sigue las instrucciones para
           continuar con la recuperación de la contraseña
         </h3>
-        <Link to="/contraseña-nueva">
+        <Link to="/login">
           <Button className="px-16">Continuar</Button>
         </Link>
       </div>
