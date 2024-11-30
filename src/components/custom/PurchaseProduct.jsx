754e19034c663b9bbf785e9f5b29be8a0a9bc764
Andres Parra <byandrev@gmail.com>
2024-11-30 17:34:51 -0500

    feat: refactor PurchaseProduct component to use react-query for product data fetching

--- src/components/custom/PurchaseProduct.jsx
+++ src/components/custom/PurchaseProduct.jsx
@@ -1,20 +1,28 @@
-import useProduct from "../../hooks/useProduct";
+import { useQuery } from "react-query";
+import CartService from "../../services/api/CartService";
 import ProductPurchase from "./ProductPurchase";
 
 export default function PurchaseProduct({ open }) {
-  const { productSelect } = useProduct();
+  const { data, isLoading, isFetching } = useQuery(
+    "products-cart",
+    CartService.getAll
+  );
 
-  if (!open || !productSelect?.length) {
+  if (isLoading || isFetching) {
+    return <p>Cargando...</p>;
+  }
+
+  if (!open || !data?.data?.length) {
     return null;
   }
 
   return (
     open &&
-    productSelect.map((product) => (
+    data?.data?.map((product) => (
       <ProductPurchase
-        key={product.id}
-        name={product.name}
-        price={product.price + ".000"}
+        key={product?.id}
+        name={product?.name}
+        price={product?.price + ".000"}
         quantity={1}
         image={"/assets/product.png"}
       />
