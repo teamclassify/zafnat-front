bc0dae430f1444213363ce5ad58df4e54bcdfd89
Andres Parra <byandrev@gmail.com>
2024-12-01 19:06:09 -0500

    feat: add create and update functions to ProductsService for product management

--- src/services/api/ProductsService.jsx
+++ src/services/api/ProductsService.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { URL, handleAxiosError } from ".";
+import { getToken } from "./Auth";
 
 async function getAll(
   { status, page, name, sizes, colors, categories } = {
@@ -50,9 +51,64 @@ async function getById(id) {
   }
 }
 
+async function create({ name, description, categories }) {
+  const token = await getToken();
+
+  if (!token) throw new Error("Token not found");
+
+  try {
+    const res = await axios({
+      url: `${URL}/products`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        name,
+        description,
+        categories,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+}
+
+async function update({ id, name, description, categories, status }) {
+  try {
+    const token = await getToken();
+
+    if (!token) throw new Error("Token not found");
+
+    const res = await axios({
+      url: `${URL}/products/${id}`,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        name,
+        description,
+        categories,
+        status,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+}
+
 const ProductsService = {
   getAll,
   getById,
+  create,
+  update,
 };
 
 export default ProductsService;
