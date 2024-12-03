abf056e3b3908414f5f295b56dcc101434cb09d0
MauricioDDS <mauriciodidonato794@gmail.com>
2024-12-03 11:37:05 -0500

    feat: upload csv production order

--- src/pages/uploadProducts/components/UploadFile.jsx
+++ src/pages/uploadProducts/components/UploadFile.jsx
@@ -1,5 +1,6 @@
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import useUpdateCSV from "../../../hooks/useUpdateCSV";
 import ContentUpload from "./ContentUpload";
 import { Format } from "./FormatGuide";
@@ -8,9 +9,26 @@ export default function UploadFile() {
   const csvProps = useUpdateCSV();
   const { handleUpload, selectedFile } = csvProps;
 
+  console.log(csvProps.data);
+
   return (
     <Card>
       <CardContent className="p-4">
+        {csvProps?.data && csvProps.data.status === 400 && (
+          <Alert className="mb-4">
+            <AlertTitle className="font-bold">Ocurrio un error!</AlertTitle>
+            <AlertDescription>{csvProps.data.msg}</AlertDescription>
+          </Alert>
+        )}
+
+        {csvProps?.data && csvProps.data.status === 200 && (
+          <Alert className="mb-4">
+            <AlertTitle className="font-bold">
+              Orden subida correctamente!
+            </AlertTitle>
+          </Alert>
+        )}
+
         <ContentUpload {...csvProps} />
       </CardContent>
       <div className="flex items-center justify-center">
@@ -18,7 +36,7 @@ export default function UploadFile() {
       </div>
       <CardFooter className="flex items-end justify-end">
         <Button size="lg" onClick={handleUpload} disabled={!selectedFile}>
-          Subir
+          {csvProps.isLoading ? "Subiendo..." : "Subir"}
         </Button>
       </CardFooter>
     </Card>
